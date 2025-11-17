13c2c8f2db9794a4b2d449697e156a90a663469a
gabrielmaz <gabemaz@icloud.com>
2025-11-17 14:48:35 +0000

    wrap off, working commit

--- CAPyle_releaseV2/release/ca_descriptions/forest.py
+++ CAPyle_releaseV2/release/ca_descriptions/forest.py
@@ -259,6 +259,7 @@ def setup(args):
     config.title = "Forest fire"
     config.dimensions = 2
     config.states = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+    config.wrap = False
     # ------------------------------------------------------------------------
 
     # ---- Override the defaults below (these may be changed at anytime) ----
